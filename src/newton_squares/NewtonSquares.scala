ae61427babae7b81bb2c6e62b97c866710305e4a
Kirill Maksimov <k.maksimov@applicaster.com>
2013-03-07 01:30:13 +0200

    Newton

--- src/newton_squares/NewtonSquares.scala
+++ src/newton_squares/NewtonSquares.scala
@@ -1,20 +1,12 @@
 package newton_squares
+import Math.abs
 
 object NewtonSquares extends App {
-  def abs(x: Double) = if (x > 0) x else -x
-
-  def isGoodEnough(guess: Double, x: Double) = {
-    abs(guess * guess - x) < 0.001
-  }
-
-  def improve(guess: Double, x: Double) = {
-    (x / guess + guess) / 2
-  }
-
   def squares(guess: Double, x: Double):Double =
-    if (isGoodEnough(guess, x)) guess
-    else squares(improve(guess, x), x)
+    if (abs(guess * guess - x) < 0.001) guess
+    else squares((x / guess + guess) / 2, x)
 
   val squareOfTwo = squares(1.0, 2.0)
+  println(squareOfTwo)
   assert(squareOfTwo == 1.4142156862745097)
 }
