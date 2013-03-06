52a7986362e441711b27922af6a307d223e514d0
Kirill Maksimov <k.maksimov@applicaster.com>
2013-03-07 01:31:47 +0200

    Newton

--- src/newton_squares/NewtonSquares.scala
+++ src/newton_squares/NewtonSquares.scala
@@ -2,11 +2,11 @@ package newton_squares
 import Math.abs
 
 object NewtonSquares extends App {
-  def squares(guess: Double, x: Double):Double =
+  def squares(x: Double, guess: Double = 1):Double =
     if (abs(guess * guess - x) < 0.001) guess
-    else squares((x / guess + guess) / 2, x)
+    else squares(x, (x / guess + guess) / 2)
 
-  val squareOfTwo = squares(1.0, 2.0)
+  val squareOfTwo = squares(2)
   println(squareOfTwo)
   assert(squareOfTwo == 1.4142156862745097)
 }
