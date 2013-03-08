0c68b4c55748e274b2d66dbeb2d731c1b58a43a0
Kirill Maksimov <k.maksimov@applicaster.com>
2013-03-09 01:26:25 +0200

    Finally

--- src/curry/Curry.scala
+++ src/curry/Curry.scala
@@ -1,21 +1,17 @@
 package curry
 
 object Curry extends App {
-  def sum(f: Int => Int)(a: Int, b: Int):Int = {
-    if (a > b) 0
-    else f(a) + sum(f)(a + 1, b)
+  def mapReduce(value: Int, combine: (Int, Int) => Int)(f: Int => Int)(a: Int, b: Int):Int = {
+    if (a > b) value
+    else combine(f(a), mapReduce(value, combine)(f)(a + 1, b))
   }
 
-  assert(sum(x => x)(2, 5) == 14)
+  def sum(a: Int, b: Int):Int = mapReduce(0, (x, y) => x + y)(x => x)(a, b)
+  assert(sum(2, 5) == 14)
 
-  def product(f: Int => Int)(a: Int, b: Int):Int = {
-    if (a > b) 1
-    else f(a) * product(f)(a + 1, b)
-  }
-
+  def product(f: Int => Int)(a: Int, b: Int):Int = mapReduce(1, (x, y) => x * y)(f)(a, b)
   assert(product(x => x * x)(3, 7) == 6350400)
 
   def factorial(n: Int) = product(x => x)(1, n)
-
   assert(factorial(4) == 24)
 }
