cd714fa472b77d4c3c7a08071111d8b21be3f5d9
Kirill Maksimov <k.maksimov@applicaster.com>
2013-03-09 01:07:11 +0200

    Sum

--- src/curry/Curry.scala
+++ src/curry/Curry.scala
@@ -1,6 +1,13 @@
 package curry
 
 object Curry extends App {
+  def sum(f: Int => Int)(a: Int, b: Int):Int = {
+    if (a > b) 0
+    else f(a) + sum(f)(a + 1, b)
+  }
+
+  assert(sum(x => x)(2, 5) == 14)
+
   def product(f: Int => Int)(a: Int, b: Int):Int = {
     if (a > b) 1
     else f(a) * product(f)(a + 1, b)
