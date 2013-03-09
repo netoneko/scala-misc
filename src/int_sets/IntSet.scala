d311eae2025d68df625f480948ca28294af33d81
Kirill Maksimov <k.maksimov@applicaster.com>
2013-03-09 15:09:51 +0200

    int sets

---
+++ src/int_sets/IntSet.scala
@@ -0,0 +1,40 @@
+package int_sets
+
+abstract class IntSet {
+  def incl(x: Int): IntSet
+  def contains(x: Int): Boolean
+}
+
+class Empty extends IntSet {
+  def incl(x: Int): IntSet = new NonEmpty(x)
+
+  def contains(x: Int):Boolean = false
+}
+
+class NonEmpty(v: Int, leftSet: IntSet, rightSet: IntSet) extends IntSet {
+  def this(v: Int) = this(v, new Empty, new Empty)
+
+  val value = v
+  var left = leftSet
+  var right = rightSet
+
+  def contains(x: Int):Boolean = if (this.value == x) true else left.contains(x) || right.contains(x)
+
+  def incl(x: Int):IntSet = {
+    if (this.contains(x)) this
+    else if (this.value < x) { right = new NonEmpty(x) ; right }
+    else { left = new NonEmpty(x, new Empty, new Empty); left }
+  }
+}
+
+object Test extends App {
+  val set = new NonEmpty(2)
+  assert(set contains 2)
+
+  set.incl(3).incl(1)
+
+  assert(set contains 2)
+  assert(set contains 1)
+
+  assert(!set.contains(99))
+}
